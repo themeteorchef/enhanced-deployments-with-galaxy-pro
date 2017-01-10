d7c49e0123a0a069b05e42d2216c98b1f797a0db
Ryan Glover <me@ryanglover.net>
2017-01-10 08:09:31 -0600

    make fixtures dev only and update production deploy script

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -3,37 +3,39 @@ import { Roles } from 'meteor/alanning:roles';
 import { Accounts } from 'meteor/accounts-base';
 import Projects from '../../api/projects/projects';
 
-const users = [{
-  email: 'admin@admin.com',
-  password: 'password',
-  profile: {
-    name: { first: 'Carl', last: 'Winslow' },
-  },
-  roles: ['admin'],
-}];
+if (Meteor.isDevelopment) {
+  const users = [{
+    email: 'admin@admin.com',
+    password: 'password',
+    profile: {
+      name: { first: 'Carl', last: 'Winslow' },
+    },
+    roles: ['admin'],
+  }];
 
-users.forEach(({ email, password, profile, roles }) => {
-  const userExists = Meteor.users.findOne({ 'emails.address': email });
+  users.forEach(({ email, password, profile, roles }) => {
+    const userExists = Meteor.users.findOne({ 'emails.address': email });
 
-  if (!userExists) {
-    const userId = Accounts.createUser({ email, password, profile });
-    Roles.addUsersToRoles(userId, roles);
-  }
-});
+    if (!userExists) {
+      const userId = Accounts.createUser({ email, password, profile });
+      Roles.addUsersToRoles(userId, roles);
+    }
+  });
 
-const projectsCount = 100;
-let i = 0;
+  const projectsCount = 100;
+  let i = 0;
 
-if (Projects.find().count() < projectsCount) {
-  while (i < projectsCount) {
-    Projects.insert({
-      owner: Meteor.users.findOne()._id,
-      title: `Project #${i}`,
-      url: 'https://google.com',
-      image: 'http://fillmurray.com/500/300',
-      description: `Project #${i}'s description. Wheeeeee.`,
-      createdBy: i % 5 ? 'Bart Simpson' : 'Homer Simpson',
-    });
-    i += 1;
+  if (Projects.find().count() < projectsCount) {
+    while (i < projectsCount) {
+      Projects.insert({
+        owner: Meteor.users.findOne()._id,
+        title: `Project #${i}`,
+        url: 'https://google.com',
+        image: 'http://fillmurray.com/500/300',
+        description: `Project #${i}'s description. Wheeeeee.`,
+        createdBy: i % 5 ? 'Bart Simpson' : 'Homer Simpson',
+      });
+      i += 1;
+    }
   }
 }
